54f22018beace50f1784ecb0881811eaf9b24b5a
lavit797 <lavittyagi2004@gmail.com>
2026-08-30 16:25:18 +0530

    adding comment for data base connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+// data base connection
 async function connectDB() {
   if (!process.env.MONGO_URI) throw new Error("MONGO_URI is not configured");
   mongoose.set("strictQuery", true);
